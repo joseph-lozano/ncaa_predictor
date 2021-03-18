74d453803b32a666da12d1fabf39a819d5c20ffd
Joseph Lozano <joseph@hey.com>
2021-03-17 22:58:34 -0400

    get it to deploy

--- lib/application.ex
+++ lib/application.ex
@@ -4,13 +4,21 @@ defmodule NCAA.Application do
   require Logger
 
   def start(_type, _args) do
+    port =
+      case Mix.env() do
+        :prod ->
+          80
+
+        _ ->
+          Logger.info("Starting application at http://localhost:4000")
+          4000
+      end
+
     children = [
-      {Plug.Cowboy, scheme: :http, plug: NCAA.Server, options: [port: 4000]}
+      {Plug.Cowboy, scheme: :http, plug: NCAA.Server, options: [port: port]}
     ]
 
     opts = [strategy: :one_for_one, name: NCAA.Supervisor]
-
-    Logger.info("Starting application at http://localhost:4000")
 
     Supervisor.start_link(children, opts)
   end
